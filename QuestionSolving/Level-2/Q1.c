8fbef0ea03ed460762dccbc07b2b4f67d5402ef0
teavostok <teavostok1@gmail.com>
2026-05-16 15:53:48 +0530

    Added Q1L-2(Prime Number Checker)

---
+++ QuestionSolving/Level-2/Q1.c
@@ -0,0 +1,27 @@
+//Q1L-2 : Prime Number Check
+
+#include <stdio.h>
+
+int main(){
+    
+    int number;
+    int base = 2;
+    int temp = 0;
+    printf("Please enter the number to check if its prime: " );
+    scanf("%d", &number);
+    
+    while(base < number){
+        
+        temp = number % base;
+        
+        if(temp == 0){
+            printf("The number is not a Prime number. It can be divided by %d", base);
+            break;
+        }
+        else{
+            ++base;
+            continue;
+        }
+    }
+    printf("Search completed... %d is NOT a prime number", number);
+}
